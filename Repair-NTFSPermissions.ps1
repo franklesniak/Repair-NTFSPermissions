0638b84e9a275eae07444dc8447483819d0e5639
Frank Lesniak <franklesniak@hotmail.com>
2024-12-17 16:24:45 -0600

    Cleaned up Remove-SpecificAccessRuleRobust

--- Repair-NTFSPermissions.ps1
+++ Repair-NTFSPermissions.ps1
@@ -4448,84 +4448,271 @@ function Test-ValidSID {
 }
 
 function Remove-SpecificAccessRuleRobust {
-    #region FunctionHeader #########################################################
+    # .SYNOPSIS
+    # Removes an access rule from an object.
+    #
+    # .DESCRIPTION
     # This function removes a specific access rule from a
-    # System.Security.AccessControl.DirectorySecurity or similar object safely and in a
-    # way that automaticaly retries if an error occurs.
+    # System.Security.AccessControl.DirectorySecurity or similar object "safely"
+    # (i.e., without throwing any errors should the process fail) and in a way that
+    # automaticaly retries if an error occurs.
     #
-    # Four positional arguments are required:
+    # .PARAMETER CurrentAttemptNumber
+    # This parameter is required; it is an integer indicating the current attempt
+    # number. When calling this function for the first time, it should be 1.
     #
-    # The first argument is an integer indicating the current attempt number. When
-    # calling this function for the first time, it should be 1
+    # .PARAMETER MaxAttempts
+    # This parameter is required; it is an integer representing the maximum number
+    # of attempts that the function will observe before giving up.
     #
-    # The second argument is an integer representing the maximum number of attempts that
-    # the function will observe before giving up
+    # .PARAMETER ReferenceToAccessControlListObject
+    # This parameter is required; it is a reference to a
+    # System.Security.AccessControl.DirectorySecurity or similar object from
+    # which the access control entry will be removed.
     #
-    # The third argument is a reference to a
-    # System.Security.AccessControl.DirectorySecurity or similar object that the access
-    # control entry will be removed from
+    # .PARAMETER ReferenceToAccessRuleObject
+    # This parameter is required; it is a reference to a
+    # System.Security.AccessControl.FileSystemAccessRule or similar object that
+    # will be removed from the access control list.
     #
-    # The fourth argument is a reference to a
-    # System.Security.AccessControl.FileSystemAccessRule or similar object that will be
-    # removed from the access control list
+    # .EXAMPLE
+    # $item = Get-Item 'D:\Shared\Human_Resources'
+    # $directorySecurity = $item.GetAccessControl()
+    # $arrFileSystemAccessRules = @($directorySecurity.Access)
+    # $boolSuccess = Remove-SpecificAccessRuleRobust -CurrentAttemptNumber 1 -MaxAttempts 8 -ReferenceToAccessControlListObject ([ref]$directorySecurity) -ReferenceToAccessRuleObject ([ref]($arrFileSystemAccessRules[0]))
     #
-    # The function returns $true if the process completed successfully; $false
-    # otherwise
-    #
-    # Example usage:
+    # .EXAMPLE
     # $item = Get-Item 'D:\Shared\Human_Resources'
     # $directorySecurity = $item.GetAccessControl()
     # $arrFileSystemAccessRules = @($directorySecurity.Access)
     # $boolSuccess = Remove-SpecificAccessRuleRobust 1 8 ([ref]$directorySecurity) ([ref]($arrFileSystemAccessRules[0]))
     #
-    # Version: 1.0.20230731.0
-    #endregion FunctionHeader #########################################################
+    # .INPUTS
+    # None. You can't pipe objects to Remove-SpecificAccessRuleRobust.
+    #
+    # .OUTPUTS
+    # System.Boolean. Remove-SpecificAccessRuleRobust returns a boolean value
+    # indiciating whether the process completed successfully. $true means the
+    # process completed successfully; $false means there was an error.
+    #
+    # .NOTES
+    # This function also supports the use of arguments, which can be used
+    # instead of parameters. If arguments are used instead of parameters, then
+    # four positional arguments are required:
+    #
+    # The first argument is an integer indicating the current attempt number. When
+    # calling this function for the first time, it should be 1.
+    #
+    # The second argument is an integer representing the maximum number of attempts
+    # that the function will observe before giving up.
+    #
+    # The third argument is a reference to a
+    # System.Security.AccessControl.DirectorySecurity or similar object from which
+    # the access control entry will be removed.
+    #
+    # The fourth argument is a reference to a
+    # System.Security.AccessControl.FileSystemAccessRule or similar object that
+    # will be removed from the access control list.
+    #
+    # Version: 1.1.20241217.0
 
-    #region License ################################################################
-    # Copyright (c) 2023 Frank Lesniak
+    #region License ############################################################
+    # Copyright (c) 2024 Frank Lesniak
     #
-    # Permission is hereby granted, free of charge, to any person obtaining a copy of
-    # this software and associated documentation files (the "Software"), to deal in the
-    # Software without restriction, including without limitation the rights to use,
-    # copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
-    # Software, and to permit persons to whom the Software is furnished to do so,
-    # subject to the following conditions:
+    # Permission is hereby granted, free of charge, to any person obtaining a copy
+    # of this software and associated documentation files (the "Software"), to deal
+    # in the Software without restriction, including without limitation the rights
+    # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    # copies of the Software, and to permit persons to whom the Software is
+    # furnished to do so, subject to the following conditions:
     #
-    # The above copyright notice and this permission notice shall be included in all
-    # copies or substantial portions of the Software.
+    # The above copyright notice and this permission notice shall be included in
+    # all copies or substantial portions of the Software.
     #
     # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-    # FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-    # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
-    # AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-    # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-    #endregion License ################################################################
+    # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    # SOFTWARE.
+    #endregion License ############################################################
+
+    param (
+        [int]$CurrentAttemptNumber = 1,
+        [int]$MaxAttempts = 1,
+        [ref]$ReferenceToAccessControlListObject = ([ref]$null),
+        [ref]$ReferenceToAccessRuleObject = ([ref]$null)
+    )
+
+    #region FunctionsToSupportErrorHandling ####################################
+    function Get-ReferenceToLastError {
+        #region FunctionHeader #############################################
+        # Function returns $null if no errors on on the $error stack;
+        # Otherwise, function returns a reference (memory pointer) to the last
+        # error that occurred.
+        #
+        # Version: 1.0.20241211.0
+        #endregion FunctionHeader #############################################
+
+        #region License ####################################################
+        # Copyright (c) 2024 Frank Lesniak
+        #
+        # Permission is hereby granted, free of charge, to any person obtaining
+        # a copy of this software and associated documentation files (the
+        # "Software"), to deal in the Software without restriction, including
+        # without limitation the rights to use, copy, modify, merge, publish,
+        # distribute, sublicense, and/or sell copies of the Software, and to
+        # permit persons to whom the Software is furnished to do so, subject to
+        # the following conditions:
+        #
+        # The above copyright notice and this permission notice shall be
+        # included in all copies or substantial portions of the Software.
+        #
+        # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+        # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+        # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+        # BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+        # ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+        # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        # SOFTWARE.
+        #endregion License ####################################################
+
+        #region DownloadLocationNotice #####################################
+        # The most up-to-date version of this script can be found on the
+        # author's GitHub repository at:
+        # https://github.com/franklesniak/PowerShell_Resources
+        #endregion DownloadLocationNotice #####################################
+
+        if ($Error.Count -gt 0) {
+            return ([ref]($Error[0]))
+        } else {
+            return $null
+        }
+    }
+
+    function Test-ErrorOccurred {
+        #region FunctionHeader #############################################
+        # Function accepts two positional arguments:
+        #
+        # The first argument is a reference (memory pointer) to the last error
+        # that had occurred prior to calling the command in question - that is,
+        # the command that we want to test to see if an error occurred.
+        #
+        # The second argument is a reference to the last error that had
+        # occurred as-of the completion of the command in question.
+        #
+        # Function returns $true if it appears that an error occurred; $false
+        # otherwise
+        #
+        # Version: 1.0.20241211.0
+        #endregion FunctionHeader #############################################
+
+        #region License ####################################################
+        # Copyright (c) 2024 Frank Lesniak
+        #
+        # Permission is hereby granted, free of charge, to any person obtaining
+        # a copy of this software and associated documentation files (the
+        # "Software"), to deal in the Software without restriction, including
+        # without limitation the rights to use, copy, modify, merge, publish,
+        # distribute, sublicense, and/or sell copies of the Software, and to
+        # permit persons to whom the Software is furnished to do so, subject to
+        # the following conditions:
+        #
+        # The above copyright notice and this permission notice shall be
+        # included in all copies or substantial portions of the Software.
+        #
+        # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+        # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+        # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+        # BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+        # ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+        # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        # SOFTWARE.
+        #endregion License ####################################################
+
+        #region DownloadLocationNotice #####################################
+        # The most up-to-date version of this script can be found on the
+        # author's GitHub repository at:
+        # https://github.com/franklesniak/PowerShell_Resources
+        #endregion DownloadLocationNotice #####################################
+
+        # TO-DO: Validate input
+
+        $boolErrorOccurred = $false
+        if (($null -ne ($args[0])) -and ($null -ne ($args[1]))) {
+            # Both not $null
+            if ((($args[0]).Value) -ne (($args[1]).Value)) {
+                $boolErrorOccurred = $true
+            }
+        } else {
+            # One is $null, or both are $null
+            # NOTE: ($args[0]) could be non-null, while ($args[1])
+            # could be null if $error was cleared; this does not indicate an
+            # error.
+            # So:
+            # If both are null, no error
+            # If ($args[0]) is null and ($args[1]) is non-null, error
+            # If ($args[0]) is non-null and ($args[1]) is null, no error
+            if (($null -eq ($args[0])) -and ($null -ne ($args[1]))) {
+                $boolErrorOccurred = $true
+            }
+        }
+
+        return $boolErrorOccurred
+    }
+    #endregion FunctionsToSupportErrorHandling ####################################
 
     trap {
         # Intentionally left empty to prevent terminating errors from halting
         # processing
     }
 
-    $intCurrentAttemptNumber = $args[0]
-    $intMaximumAttempts = $args[1]
-    $refAccessControlSecurity = $args[2]
-    $refAccessControlAccessRule = $args[3]
+    #region Assign Parameters and Arguments to Internally-Used Variables #######
+    $boolUseArguments = $false
+    if ($args.Count -eq 4) {
+        # Arguments may have been supplied instead of parameters
+        if (($CurrentAttemptNumber -eq 1) -and ($MaxAttempts -eq 1) -and ($null -eq $ReferenceToAccessControlListObject.Value) -and ($null -eq $ReferenceToAccessRuleObject.Value)) {
+            # Parameters all match default values, so it's safe to say that
+            # arguments were used instead of parameters
+            $boolUseArguments = $true
+        }
+    }
+
+    if (-not $boolUseArguments) {
+        # Use parameters
+        $intCurrentAttemptNumber = $CurrentAttemptNumber
+        $intMaximumAttempts = $MaxAttempts
+        $refAccessControlSecurity = $ReferenceToAccessControlListObject
+        $refAccessControlAccessRule = $ReferenceToAccessRuleObject
+    } else {
+        # Use positional arguments
+        $intCurrentAttemptNumber = $args[0]
+        $intMaximumAttempts = $args[1]
+        $refAccessControlSecurity = $args[2]
+        $refAccessControlAccessRule = $args[3]
+    }
+    #endregion Assign Parameters and Arguments to Internally-Used Variables #######
+
+    # TODO: Validate input
 
     # Retrieve the newest error on the stack prior to doing work
     $refLastKnownError = Get-ReferenceToLastError
 
-    # Store current error preference; we will restore it after we do the work of this
-    # function
+    # Store current error preference; we will restore it after we do the work of
+    # this function
     $actionPreferenceFormerErrorPreference = $global:ErrorActionPreference
 
-    # Set ErrorActionPreference to SilentlyContinue; this will suppress error output.
-    # Terminating errors will not output anything, kick to the empty trap statement and
-    # then continue on. Likewise, non-terminating errors will also not output anything,
-    # but they do not kick to the trap statement; they simply continue on.
+    # Set ErrorActionPreference to SilentlyContinue; this will suppress error
+    # output. Terminating errors will not output anything, kick to the empty trap
+    # statement and then continue on. Likewise, non-terminating errors will also
+    # not output anything, but they do not kick to the trap statement; they simply
+    # continue on.
     $global:ErrorActionPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
 
-    # Do the work of this function...
+    # Remove the access rule/access control entry from the list
     ($refAccessControlSecurity.Value).RemoveAccessRuleSpecific($refAccessControlAccessRule.Value)
 
     # Restore the former error preference
@@ -4539,18 +4726,14 @@ function Remove-SpecificAccessRuleRobust {
         if ($intCurrentAttemptNumber -lt $intMaximumAttempts) {
             Start-Sleep -Seconds ([math]::Pow(2, $intCurrentAttemptNumber))
 
-            $objResultIndicator = Remove-SpecificAccessRuleRobust ($intCurrentAttemptNumber + 1) $intMaximumAttempts $refAccessControlSecurity $refAccessControlAccessRule
-            $objResultIndicator
+            $objResultIndicator = Remove-SpecificAccessRuleRobust -CurrentAttemptNumber ($intCurrentAttemptNumber + 1) -MaxAttempts $intMaximumAttempts -ReferenceToAccessControlListObject $refAccessControlSecurity -ReferenceToAccessRuleObject $refAccessControlAccessRule
+            return $objResultIndicator
         } else {
-            # Number of attempts exceeded maximum
-
-            # Return failure indicator:
+            # Number of attempts exceeded maximum; return failure indicator:
             return $false
         }
     } else {
-        # No error occurred
-
-        # Return success indicator:
+        # No error occurred; return success indicator:
         return $true
     }
 }
@@ -5509,7 +5692,7 @@ function Repair-NTFSPermissionsRecursively {
                                         Write-Warning ('Removing ACE from path "' + $strThisObjectPath + '" that was already been used in an operation.')
                                     }
                                     Write-Verbose ('Removing unresolved SID "' + ($arrWorkingACEs[$intCounterA]).IdentityReference.Value + '" from path "' + $strThisObjectPath + '".')
-                                    $boolSuccess = Remove-SpecificAccessRuleRobust 1 2 ([ref]$objThisFolderPermission) ([ref]($arrWorkingACEs[$intCounterA]))
+                                    $boolSuccess = Remove-SpecificAccessRuleRobust -CurrentAttemptNumber 1 -MaxAttempts 2 -ReferenceToAccessControlListObject ([ref]$objThisFolderPermission) -ReferenceToAccessRuleObject ([ref]($arrWorkingACEs[$intCounterA]))
                                     if ($boolSuccess -eq $false) {
                                         # Permissions were not removed
                                         Write-Verbose ('...the unresolved SID was not removed (.NET call failed)...')
@@ -5546,7 +5729,7 @@ function Repair-NTFSPermissionsRecursively {
                                     $propagationFlags = ($arrWorkingACEs[$intCounterA]).PropagationFlags
                                     $SID = New-Object System.Security.Principal.SecurityIdentifier(($arrWorkingACEs[$intCounterA]).IdentityReference.Value)
                                     $fileSystemAccessRuleOld = New-Object System.Security.AccessControl.FileSystemAccessRule($SID, $fileSystemRights, $inheritanceFlags, $propagationFlags, $accessControlType)
-                                    $boolSuccess = Remove-SpecificAccessRuleRobust 1 2 ([ref]$objThisFolderPermission) ([ref]($fileSystemAccessRuleOld))
+                                    $boolSuccess = Remove-SpecificAccessRuleRobust -CurrentAttemptNumber 1 -MaxAttempts 2 -ReferenceToAccessControlListObject ([ref]$objThisFolderPermission) -ReferenceToAccessRuleObject ([ref]($fileSystemAccessRuleOld))
                                     if ($boolSuccess -eq $false) {
                                         # Permissions were not removed
                                         Write-Verbose ('...the unresolved SID was not removed (.NET call failed)...')
