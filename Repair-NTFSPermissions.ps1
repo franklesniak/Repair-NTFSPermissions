b2b73f39ae84e86254ff1d7e610a0c4579bd1b59
Frank Lesniak <franklesniak@hotmail.com>
2024-12-31 15:37:35 -0600

    Update Test-TypeNameAvailability

--- Repair-NTFSPermissions.ps1
+++ Repair-NTFSPermissions.ps1
@@ -7086,7 +7086,7 @@ function Test-TypeNameAvailability {
     # that contains the name of the type for which the function will determine
     # availability.
     #
-    # Version: 2.0.20241220.0
+    # Version: 2.0.20241223.0
 
     #region License ############################################################
     # Copyright (c) 2024 Frank Lesniak
@@ -7122,13 +7122,75 @@ function Test-TypeNameAvailability {
 
     #region FunctionsToSupportErrorHandling ####################################
     function Get-ReferenceToLastError {
-        #region FunctionHeader #############################################
-        # Function returns $null if no errors on on the $error stack;
-        # Otherwise, function returns a reference (memory pointer) to the last
-        # error that occurred.
+        # .SYNOPSIS
+        # Gets a reference (memory pointer) to the last error that occurred.
         #
-        # Version: 1.0.20241211.0
-        #endregion FunctionHeader #############################################
+        # .DESCRIPTION
+        # Returns a reference (memory pointer) to $null ([ref]$null) if no
+        # errors on on the $error stack; otherwise, returns a reference to the
+        # last error that occurred.
+        #
+        # .EXAMPLE
+        # # Intentionally empty trap statement to prevent terminating errors
+        # # from halting processing
+        # trap { }
+        #
+        # # Retrieve the newest error on the stack prior to doing work:
+        # $refLastKnownError = Get-ReferenceToLastError
+        #
+        # # Store current error preference; we will restore it after we do some
+        # # work:
+        # $actionPreferenceFormerErrorPreference = $global:ErrorActionPreference
+        #
+        # # Set ErrorActionPreference to SilentlyContinue; this will suppress
+        # # error output. Terminating errors will not output anything, kick to
+        # # the empty trap statement and then continue on. Likewise, non-
+        # # terminating errors will also not output anything, but they do not
+        # # kick to the trap statement; they simply continue on.
+        # $global:ErrorActionPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
+        #
+        # # Do something that might trigger an error
+        # Get-Item -Path 'C:\MayNotExist.txt'
+        #
+        # # Restore the former error preference
+        # $global:ErrorActionPreference = $actionPreferenceFormerErrorPreference
+        #
+        # # Retrieve the newest error on the error stack
+        # $refNewestCurrentError = Get-ReferenceToLastError
+        #
+        # $boolErrorOccurred = $false
+        # if (($null -ne $refLastKnownError.Value) -and ($null -ne $refNewestCurrentError.Value)) {
+        #     # Both not $null
+        #     if (($refLastKnownError.Value) -ne ($refNewestCurrentError.Value)) {
+        #         $boolErrorOccurred = $true
+        #     }
+        # } else {
+        #     # One is $null, or both are $null
+        #     # NOTE: $refLastKnownError could be non-null, while
+        #     # $refNewestCurrentError could be null if $error was cleared;
+        #     # this does not indicate an error.
+        #     # So:
+        #     # If both are null, no error
+        #     # If $refLastKnownError is null and $refNewestCurrentError is
+        #     # non-null, error
+        #     # If $refLastKnownError is non-null and $refNewestCurrentError is
+        #     # null, no error
+        #     if (($null -eq $refLastKnownError.Value) -and ($null -ne $refNewestCurrentError.Value)) {
+        #         $boolErrorOccurred = $true
+        #     }
+        # }
+        #
+        # .INPUTS
+        # None. You can't pipe objects to Get-ReferenceToLastError.
+        #
+        # .OUTPUTS
+        # System.Management.Automation.PSReference ([ref]).
+        # Get-ReferenceToLastError returns a reference (memory pointer) to the
+        # last error that occurred. It returns a reference to $null
+        # ([ref]$null) if there are no errors on on the $error stack.
+        #
+        # .NOTES
+        # Version: 2.0.20241223.0
 
         #region License ####################################################
         # Copyright (c) 2024 Frank Lesniak
@@ -7153,36 +7215,118 @@ function Test-TypeNameAvailability {
         # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
         # SOFTWARE.
         #endregion License ####################################################
-
-        #region DownloadLocationNotice #####################################
-        # The most up-to-date version of this script can be found on the
-        # author's GitHub repository at:
-        # https://github.com/franklesniak/PowerShell_Resources
-        #endregion DownloadLocationNotice #####################################
 
         if ($Error.Count -gt 0) {
             return ([ref]($Error[0]))
         } else {
-            return $null
+            return ([ref]$null)
         }
     }
 
     function Test-ErrorOccurred {
-        #region FunctionHeader #############################################
-        # Function accepts two positional arguments:
+        # .SYNOPSIS
+        # Checks to see if an error occurred during a time period, i.e., during
+        # the execution of a command.
         #
-        # The first argument is a reference (memory pointer) to the last error
-        # that had occurred prior to calling the command in question - that is,
-        # the command that we want to test to see if an error occurred.
+        # .DESCRIPTION
+        # Using two references (memory pointers) to errors, this function
+        # checks to see if an error occurred based on differences between the
+        # two errors.
         #
-        # The second argument is a reference to the last error that had
-        # occurred as-of the completion of the command in question.
+        # To use this function, you must first retrieve a reference to the last
+        # error that occurred prior to the command you are about to run. Then,
+        # run the command. After the command completes, retrieve a reference to
+        # the last error that occurred. Pass these two references to this
+        # function to determine if an error occurred.
         #
-        # Function returns $true if it appears that an error occurred; $false
-        # otherwise
+        # .PARAMETER ReferenceToEarlierError
+        # This parameter is required; it is a reference (memory pointer) to a
+        # System.Management.Automation.ErrorRecord that represents the newest
+        # error on the stack earlier in time, i.e., prior to running the
+        # command for which you wish to determine whether an error occurred.
         #
-        # Version: 1.0.20241211.0
-        #endregion FunctionHeader #############################################
+        # If no error was on the stack at this time, ReferenceToEarlierError
+        # must be a reference to $null ([ref]$null).
+        #
+        # .PARAMETER ReferenceToLaterError
+        # This parameter is required; it is a reference (memory pointer) to a
+        # System.Management.Automation.ErrorRecord that represents the newest
+        # error on the stack later in time, i.e., after to running the command
+        # for which you wish to determine whether an error occurred.
+        #
+        # If no error was on the stack at this time, ReferenceToLaterError
+        # must be a reference to $null ([ref]$null).
+        #
+        # .EXAMPLE
+        # # Intentionally empty trap statement to prevent terminating errors
+        # # from halting processing
+        # trap { }
+        #
+        # # Retrieve the newest error on the stack prior to doing work
+        # if ($Error.Count -gt 0) {
+        #     $refLastKnownError = ([ref]($Error[0]))
+        # } else {
+        #     $refLastKnownError = ([ref]$null)
+        # }
+        #
+        # # Store current error preference; we will restore it after we do some
+        # # work:
+        # $actionPreferenceFormerErrorPreference = $global:ErrorActionPreference
+        #
+        # # Set ErrorActionPreference to SilentlyContinue; this will suppress
+        # # error output. Terminating errors will not output anything, kick to
+        # # the empty trap statement and then continue on. Likewise, non-
+        # # terminating errors will also not output anything, but they do not
+        # # kick to the trap statement; they simply continue on.
+        # $global:ErrorActionPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
+        #
+        # # Do something that might trigger an error
+        # Get-Item -Path 'C:\MayNotExist.txt'
+        #
+        # # Restore the former error preference
+        # $global:ErrorActionPreference = $actionPreferenceFormerErrorPreference
+        #
+        # # Retrieve the newest error on the error stack
+        # if ($Error.Count -gt 0) {
+        #     $refNewestCurrentError = ([ref]($Error[0]))
+        # } else {
+        #     $refNewestCurrentError = ([ref]$null)
+        # }
+        #
+        # if (Test-ErrorOccurred -ReferenceToEarlierError $refLastKnownError -ReferenceToLaterError $refNewestCurrentError) {
+        #     # Error occurred
+        # } else {
+        #     # No error occurred
+        # }
+        #
+        # .INPUTS
+        # None. You can't pipe objects to Test-ErrorOccurred.
+        #
+        # .OUTPUTS
+        # System.Boolean. Test-ErrorOccurred returns a boolean value indicating
+        # whether an error occurred during the time period in question. $true
+        # indicates an error occurred; $false indicates no error occurred.
+        #
+        # .NOTES
+        # This function also supports the use of positional parameters instead
+        # of named parameters. If positional parameters are used intead of
+        # named parameters, then two positional parameters are required:
+        #
+        # The first positional parameter is a reference (memory pointer) to a
+        # System.Management.Automation.ErrorRecord that represents the newest
+        # error on the stack earlier in time, i.e., prior to running the
+        # command for which you wish to determine whether an error occurred. If
+        # no error was on the stack at this time, the first positional
+        # parameter must be a reference to $null ([ref]$null).
+        #
+        # The second positional parameter is a reference (memory pointer) to a
+        # System.Management.Automation.ErrorRecord that represents the newest
+        # error on the stack later in time, i.e., after to running the command
+        # for which you wish to determine whether an error occurred. If no
+        # error was on the stack at this time, ReferenceToLaterError must be
+        # a reference to $null ([ref]$null).
+        #
+        # Version: 2.0.20241223.0
 
         #region License ####################################################
         # Copyright (c) 2024 Frank Lesniak
@@ -7207,31 +7351,31 @@ function Test-TypeNameAvailability {
         # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
         # SOFTWARE.
         #endregion License ####################################################
+        param (
+            [ref]$ReferenceToEarlierError = ([ref]$null),
+            [ref]$ReferenceToLaterError = ([ref]$null)
+        )
 
-        #region DownloadLocationNotice #####################################
-        # The most up-to-date version of this script can be found on the
-        # author's GitHub repository at:
-        # https://github.com/franklesniak/PowerShell_Resources
-        #endregion DownloadLocationNotice #####################################
-
-        # TO-DO: Validate input
+        # TODO: Validate input
 
         $boolErrorOccurred = $false
-        if (($null -ne ($args[0])) -and ($null -ne ($args[1]))) {
+        if (($null -ne $ReferenceToEarlierError.Value) -and ($null -ne $ReferenceToLaterError.Value)) {
             # Both not $null
-            if ((($args[0]).Value) -ne (($args[1]).Value)) {
+            if (($ReferenceToEarlierError.Value) -ne ($ReferenceToLaterError.Value)) {
                 $boolErrorOccurred = $true
             }
         } else {
             # One is $null, or both are $null
-            # NOTE: ($args[0]) could be non-null, while ($args[1])
-            # could be null if $error was cleared; this does not indicate an
-            # error.
+            # NOTE: $ReferenceToEarlierError could be non-null, while
+            # $ReferenceToLaterError could be null if $error was cleared; this
+            # does not indicate an error.
             # So:
-            # If both are null, no error
-            # If ($args[0]) is null and ($args[1]) is non-null, error
-            # If ($args[0]) is non-null and ($args[1]) is null, no error
-            if (($null -eq ($args[0])) -and ($null -ne ($args[1]))) {
+            # - If both are null, no error
+            # - If $ReferenceToEarlierError is null and $ReferenceToLaterError
+            #   is non-null, error
+            # - If $ReferenceToEarlierError is non-null and
+            #   $ReferenceToLaterError is null, no error
+            if (($null -eq $ReferenceToEarlierError.Value) -and ($null -ne $ReferenceToLaterError.Value)) {
                 $boolErrorOccurred = $true
             }
         }
@@ -7273,7 +7417,7 @@ function Test-TypeNameAvailability {
     # Retrieve the newest error on the error stack
     $refNewestCurrentError = Get-ReferenceToLastError
 
-    if (Test-ErrorOccurred $refLastKnownError $refNewestCurrentError) {
+    if (Test-ErrorOccurred -ReferenceToEarlierError $refLastKnownError -ReferenceToLaterError $refNewestCurrentError) {
         # Error occurred; we're not returning a failure indicator, though.
 
         return $boolTypeNameAvailable
