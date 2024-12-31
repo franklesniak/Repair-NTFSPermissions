16ef2c819a3993261ac308b4ade14817222207ed
Frank Lesniak <franklesniak@hotmail.com>
2024-12-31 14:55:40 -0600

    Update Test-Windows

--- Repair-NTFSPermissions.ps1
+++ Repair-NTFSPermissions.ps1
@@ -527,73 +527,107 @@ function Get-PSVersion {
 }
 
 function Test-Windows {
-    #region FunctionHeader #################################################
-    # Returns a boolean ($true or $false) indicating whether the current
-    # PowerShell session is running on Windows. This function is useful for
-    # writing scripts that need to behave differently on Windows and non-
-    # Windows platforms (Linux, macOS, etc.). Additionally, this function is
-    # useful because it works on Windows PowerShell 1.0 through 5.1, which do
-    # not have the $IsWindows global variable.
+    # .SYNOPSIS
+    # Returns $true if PowerShell is running on Windows; otherwise, returns $false.
     #
-    # Example:
+    # .DESCRIPTION
+    # Returns a boolean ($true or $false) indicating whether the current PowerShell
+    # session is running on Windows. This function is useful for writing scripts
+    # that need to behave differently on Windows and non-Windows platforms (Linux,
+    # macOS, etc.). Additionally, this function is useful because it works on
+    # Windows PowerShell 1.0 through 5.1, which do not have the $IsWindows global
+    # variable.
+    #
+    # .PARAMETER PSVersion
+    # This parameter is optional; if supplied, it must be the version number of the
+    # running version of PowerShell. If the version of PowerShell is already known,
+    # it can be passed in to this function to avoid the overhead of unnecessarily
+    # determining the version of PowerShell. If this parameter is not supplied, the
+    # function will determine the version of PowerShell that is running as part of
+    # its processing.
+    #
+    # .EXAMPLE
     # $boolIsWindows = Test-Windows
     #
-    # This example returns $true if the current PowerShell session is running
-    # on Windows, and $false if the current PowerShell session is running on a
-    # non-Windows platform (Linux, macOS, etc.)
+    # .EXAMPLE
+    # # The version of PowerShell is known to be 2.0 or above:
+    # $boolIsWindows = Test-Windows -PSVersion $PSVersionTable.PSVersion
     #
-    # Version 1.0.20241105.0
-    #endregion FunctionHeader #################################################
+    # .INPUTS
+    # None. You can't pipe objects to Test-Windows.
+    #
+    # .OUTPUTS
+    # System.Boolean. Test-Windows returns a boolean value indiciating whether
+    # PowerShell is running on Windows. $true means that PowerShell is running on
+    # Windows; $false means that PowerShell is not running on Windows.
+    #
+    # .NOTES
+    # This function also supports the use of a positional parameter instead of a
+    # named parameter. If a positional parameter is used intead of a named
+    # parameter, then one positional parameters is required: it must be the version
+    # number of the running version of PowerShell. If the version of PowerShell is
+    # already known, it can be passed in to this function to avoid the overhead of
+    # unnecessarily determining the version of PowerShell. If this parameter is not
+    # supplied, the function will determine the version of PowerShell that is
+    # running as part of its processing.
+    #
+    # Version: 1.1.20241225.0
 
-    #region License ########################################################
+    #region License ############################################################
     # Copyright (c) 2024 Frank Lesniak
     #
-    # Permission is hereby granted, free of charge, to any person obtaining a
-    # copy of this software and associated documentation files (the
-    # "Software"), to deal in the Software without restriction, including
-    # without limitation the rights to use, copy, modify, merge, publish,
-    # distribute, sublicense, and/or sell copies of the Software, and to permit
-    # persons to whom the Software is furnished to do so, subject to the
-    # following conditions:
+    # Permission is hereby granted, free of charge, to any person obtaining a copy
+    # of this software and associated documentation files (the "Software"), to deal
+    # in the Software without restriction, including without limitation the rights
+    # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    # copies of the Software, and to permit persons to whom the Software is
+    # furnished to do so, subject to the following conditions:
     #
-    # The above copyright notice and this permission notice shall be included
-    # in all copies or substantial portions of the Software.
+    # The above copyright notice and this permission notice shall be included in
+    # all copies or substantial portions of the Software.
     #
-    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-    # OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-    # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
-    # NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-    # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-    # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
-    # USE OR OTHER DEALINGS IN THE SOFTWARE.
-    #endregion License ########################################################
+    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    # SOFTWARE.
+    #endregion License ############################################################
 
-    #region DownloadLocationNotice #########################################
-    # The most up-to-date version of this script can be found on the author's
-    # GitHub repository at:
-    # https://github.com/franklesniak/PowerShell_Resources
-    #endregion DownloadLocationNotice #########################################
+    param (
+        [version]$PSVersion = ([version]'0.0')
+    )
 
     function Get-PSVersion {
-        #region FunctionHeader #################################################
-        # Returns the version of PowerShell that is running, including on the
-        # original release of Windows PowerShell (version 1.0)
+        # .SYNOPSIS
+        # Returns the version of PowerShell that is running.
         #
-        # Example:
-        # Get-PSVersion
+        # .DESCRIPTION
+        # The function outputs a [version] object representing the version of
+        # PowerShell that is running.
         #
-        # This example returns the version of PowerShell that is running. On
-        # versions of PowerShell greater than or equal to version 2.0, this
+        # On versions of PowerShell greater than or equal to version 2.0, this
         # function returns the equivalent of $PSVersionTable.PSVersion
         #
-        # The function outputs a [version] object representing the version of
-        # PowerShell that is running
-        #
         # PowerShell 1.0 does not have a $PSVersionTable variable, so this function
-        # returns [version]('1.0') on PowerShell 1.0
+        # returns [version]('1.0') on PowerShell 1.0.
         #
-        # Version 1.0.20241105.0
-        #endregion FunctionHeader #################################################
+        # .EXAMPLE
+        # $versionPS = Get-PSVersion
+        # # $versionPS now contains the version of PowerShell that is running. On
+        # # versions of PowerShell greater than or equal to version 2.0, this
+        # # function returns the equivalent of $PSVersionTable.PSVersion
+        #
+        # .INPUTS
+        # None. You can't pipe objects to Get-PSVersion.
+        #
+        # .OUTPUTS
+        # System.Version. Get-PSVersion returns a [version] value indiciating
+        # the version of PowerShell that is running.
+        #
+        # .NOTES
+        # Version: 1.0.20241225.0
 
         #region License ########################################################
         # Copyright (c) 2024 Frank Lesniak
@@ -618,12 +652,6 @@ function Test-Windows {
         # USE OR OTHER DEALINGS IN THE SOFTWARE.
         #endregion License ########################################################
 
-        #region DownloadLocationNotice #########################################
-        # The most up-to-date version of this script can be found on the author's
-        # GitHub repository at:
-        # https://github.com/franklesniak/PowerShell_Resources
-        #endregion DownloadLocationNotice #########################################
-
         if (Test-Path variable:\PSVersionTable) {
             return ($PSVersionTable.PSVersion)
         } else {
@@ -631,11 +659,19 @@ function Test-Windows {
         }
     }
 
-    $versionPS = Get-PSVersion
-    if ($versionPS.Major -ge 6) {
-        $IsWindows
+    if ($PSVersion -ne ([version]'0.0')) {
+        if ($PSVersion.Major -ge 6) {
+            return $IsWindows
+        } else {
+            return $true
+        }
     } else {
-        $true
+        $versionPS = Get-PSVersion
+        if ($versionPS.Major -ge 6) {
+            return $IsWindows
+        } else {
+            return $true
+        }
     }
 }
 
