e80e8c3bb69845a7456c61f65e209ff077330468
Frank Lesniak <franklesniak@hotmail.com>
2025-01-18 09:31:28 -0600

    Partially eliminate Write-Host

--- Repair-NTFSPermissions.ps1
+++ Repair-NTFSPermissions.ps1
@@ -10037,10 +10037,77 @@ if ($boolRemoveUnresolvedSIDs -eq $true) {
     }
 }
 
+function Get-PSVersion {
+    # .SYNOPSIS
+    # Returns the version of PowerShell that is running.
+    #
+    # .DESCRIPTION
+    # The function outputs a [version] object representing the version of
+    # PowerShell that is running.
+    #
+    # On versions of PowerShell greater than or equal to version 2.0, this
+    # function returns the equivalent of $PSVersionTable.PSVersion
+    #
+    # PowerShell 1.0 does not have a $PSVersionTable variable, so this
+    # function returns [version]('1.0') on PowerShell 1.0.
+    #
+    # .EXAMPLE
+    # $versionPS = Get-PSVersion
+    # # $versionPS now contains the version of PowerShell that is running.
+    # # On versions of PowerShell greater than or equal to version 2.0,
+    # # this function returns the equivalent of $PSVersionTable.PSVersion.
+    #
+    # .INPUTS
+    # None. You can't pipe objects to Get-PSVersion.
+    #
+    # .OUTPUTS
+    # System.Version. Get-PSVersion returns a [version] value indiciating
+    # the version of PowerShell that is running.
+    #
+    # .NOTES
+    # Version: 1.0.20250106.0
+
+    #region License ####################################################
+    # Copyright (c) 2025 Frank Lesniak
+    #
+    # Permission is hereby granted, free of charge, to any person obtaining
+    # a copy of this software and associated documentation files (the
+    # "Software"), to deal in the Software without restriction, including
+    # without limitation the rights to use, copy, modify, merge, publish,
+    # distribute, sublicense, and/or sell copies of the Software, and to
+    # permit persons to whom the Software is furnished to do so, subject to
+    # the following conditions:
+    #
+    # The above copyright notice and this permission notice shall be
+    # included in all copies or substantial portions of the Software.
+    #
+    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+    # BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+    # ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+    # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    # SOFTWARE.
+    #endregion License ####################################################
+
+    if (Test-Path variable:\PSVersionTable) {
+        return ($PSVersionTable.PSVersion)
+    } else {
+        return ([version]('1.0'))
+    }
+}
+
+$versionPS = Get-PSVersion
+
 $boolGetPSDriveWorkaroundIsKnownToBeUseful = $false
 $intReturnCode = Repair-NTFSPermissionsRecursively -WorkingPath $PathToFix -ReferenceToWhetherGetPSDriveWorkaroundIsKnownToBeUseful ([ref]$boolGetPSDriveWorkaroundIsKnownToBeUseful) -ReferenceToHashtableOfKnownSIDs ([ref]$hashtableKnownSIDs) -ReferenceToNameOfBuiltInAdministratorsGroupAccordingToTakeOwnAndICacls ([ref]$NameOfBuiltInAdministratorsGroupAccordingToTakeOwnAndICacls) -ReferenceToNameOfBuiltInAdministratorsGroupAccordingToGetAcl ([ref]$NameOfBuiltInAdministratorsGroupAccordingToGetAcl) -ReferenceToNameOfSYSTEMAccountAccordingToTakeOwnAndICacls ([ref]$NameOfSYSTEMAccountAccordingToTakeOwnAndICacls) -ReferenceToNameOfSYSTEMAccountAccordingToGetAcl ([ref]$NameOfSYSTEMAccountAccordingToGetAcl) -ReferenceToNameOfAdditionalAdministratorAccountOrGroupAccordingToTakeOwnAndICacls ([ref]$NameOfAdditionalAdministratorAccountOrGroupAccordingToTakeOwnAndICacls) -ReferenceToNameOfAdditionalAdministratorAccountOrGroupAccordingToGetAcl ([ref]$NameOfAdditionalAdministratorAccountOrGroupAccordingToGetAcl) -ReferenceToNameOfAdditionalReadOnlyAccountOrGroupAccordingToTakeOwnAndICacls ([ref]$NameOfAdditionalReadOnlyAccountOrGroupAccordingToTakeOwnAndICacls) -ReferenceToNameOfAdditionalReadOnlyAccountOrGroupAccordingToGetAcl ([ref]$NameOfAdditionalReadOnlyAccountOrGroupAccordingToGetAcl)
 if ($intReturnCode -eq 0) {
-    Write-Host ('Successfully processed path: ' + $PathToFix)
+    if ($versionPS.Major -ge 5) {
+        Write-Information ('Successfully processed path: ' + $PathToFix)
+    } else {
+        Write-Host ('Successfully processed path: ' + $PathToFix)
+    }
 } else {
     Write-Error ('There were one or more issues processing the path "' + $PathToFix + '" One error code returned was: ' + $intReturnCode)
 }
